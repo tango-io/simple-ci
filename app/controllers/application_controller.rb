d316beb0e0dec328d3ea5a8ff2462b3f0e3f3c03
Cesar Gomez <cesargomez89@gmail.com>
2013-10-18 22:49:04 -0500

    added cookies session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 require 'digest/sha1'
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :session
+  before_filter :set_session
 
   private
 
-  def session
+  def set_session
     session[:user] ||= {
       id: Digest::SHA1.hexdigest(request.remote_ip),
       expires: 1.day

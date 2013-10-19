2efdfa2322a497b5baf21a8116a51684ae2f54d6
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-19 10:41:31 -0500

    Added Job.find for worker

--- app/workers/run_tests_worker.rb
+++ app/workers/run_tests_worker.rb
@@ -2,7 +2,8 @@ class RunTestsWorker
   include Sidekiq::Worker
 
   def perform(id)
-    # TODO Add logic for run the tests and comunicate with the websocket
+    job = Job.find(id)
+    # TODO add vagrant stuff
   end
 
 end

20bf97c8a3046f6b65955694c7e23e7f6da9e5a0
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-19 02:00:23 -0500

    Added sidekiq test support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  require 'sidekiq/testing'
 
   RSpec.configure { |c| c.treat_symbols_as_metadata_keys_with_true_values = true }
 

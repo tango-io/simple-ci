2a96450e84babc8d8af84e9cf9056015cf5cfdf8
Cesar Gomez <cesargomez89@gmail.com>
2013-10-19 02:35:14 -0500

    added controller function for gemfile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+
   def index
   end
+
+  def verify_gemfile
+    @gemfile = Github.new(params[:repository])
+
+    respond_to do |format|
+      if @gemfile.is_valid?
+        format.json { render json: { script: @gemfile.script } }
+      else
+        format.json {render json: {message: 'invalid repository'}, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end

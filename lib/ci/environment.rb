d4605ef9ac99232e9a8e1c3be5bd88d98138ecf6
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-19 21:00:49 -0500

    Added tests

--- lib/ci/environment.rb
+++ lib/ci/environment.rb
@@ -1,5 +1,11 @@
 module Ci
   class Environment
 
+    attr_reader :session
+
+    def initialize
+      @session = Ci::SSH.new(user: 'simple-ci', host: '173.230.154.173')
+    end
+
   end
 end
--- spec/lib/ci/environment_spec.rb
+++ spec/lib/ci/environment_spec.rb
@@ -1,4 +1,28 @@
 require 'spec_helper'
 
-describe Ci::Environment do
+describe Ci::Environment, 'initialize' do
+
+  let(:environment) { Ci::Environment.new }
+
+  before do
+    Ci::SSH.stub(:new).and_return(double(
+      user: Faker::Name.name,
+      host: Faker::Internet.ip_v4_address
+    ))
+  end
+
+  it 'generates a session on boot' do
+    expect(environment.session).to be_present
+  end
+
+end
+
+describe Ci::Environment, 'uploader' do
+
+  let(:environment) { Ci::Environment.new }
+
+  it 'uploads a file from a given script' do
+    expect(environment.session).to be_present
+  end
+
 end

ce56dce59281675775a539479ed7af9d188cb169
Cesar Gomez <cesargomez89@gmail.com>
2013-10-23 10:34:35 -0500

    added omniauth gem routes and migrations of users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'sidekiq/web'
 
 R13Team186::Application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get "/signout" => "sessions#destroy", :as => :signout
 
   root to: 'pages#index'
   
@@ -10,6 +12,12 @@ R13Team186::Application.routes.draw do
   resources :pages, only: [:index] do
     collection do
       get :verify_gemfile
+    end
+  end
+
+  resources :users, except: [:all] do
+    member do
+      get :dashboard
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019124340) do
+ActiveRecord::Schema.define(version: 20131022235724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20131019124340) do
     t.text     "log_output"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "uid"
+    t.string   "name"
+    t.string   "nickname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "provider"
   end
 
 end

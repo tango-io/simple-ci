a7a272e661a3734282c989b28c0c24eb49e52942
Cesar Gomez <cesargomez89@gmail.com>
2013-10-23 13:42:01 -0500

    added authenticate user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,12 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def authenticate_user!
+    unless current_user
+      redirect_to :root
+      flash[:alert] = "Access dennied"
+      
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
   expose(:repos) { current_user.public_repositories }
 end

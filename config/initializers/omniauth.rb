1ebb0d627eb43919a5cb64e3b8d826ca2aa0877c
Cesar Gomez <cesargomez89@gmail.com>
2013-10-25 12:26:23 -0500

    fixed github credentials environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '61d06f186a48d777435b', '3930add738759d3f3c677a69be9c6d82266b552a'
+  CONFIG = YAML.load_file("#{Rails.root}/config/github_credentials.yml")[Rails.env]
+  provider :github, CONFIG['client_id'], CONFIG['client_secret']
 end

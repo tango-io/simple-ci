29b7fb1cc5a7d6bd5b8f79281db80c1b4fa7790b
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-28 11:30:14 -0600

    Fixed name of file

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -2,7 +2,7 @@
 # This app is going to use another websocket provider
 # in the near future.
 #
-key = YAML.load_file("#{Rails.root}/config/github_credentials.yml")[Rails.env]
+key = YAML.load_file("#{Rails.root}/config/pusher.yml")
 
 Pusher.app_id = keys['app_id']
 Pusher.key = keys['key']

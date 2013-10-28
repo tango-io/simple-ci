287302f7d5210c3196f6afb5ccb46bfc0808af01
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-28 11:27:35 -0600

    Changed pusher keys

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,9 @@
-Pusher.app_id = '57274'
-Pusher.key = 'ed310d3e49f3ba4216a8'
-Pusher.secret = '0f0833bf64d321aa8140'
+#
+# This app is going to use another websocket provider
+# in the near future.
+#
+key = YAML.load_file("#{Rails.root}/config/github_credentials.yml")[Rails.env]
+
+Pusher.app_id = keys['app_id']
+Pusher.key = keys['key']
+Pusher.secret = keys['secret']

173f92dcbe20945b44e6d7a7c8ea39ecbf4c8ba2
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-28 11:31:48 -0600

    Added constant

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -2,8 +2,8 @@
 # This app is going to use another websocket provider
 # in the near future.
 #
-key = YAML.load_file("#{Rails.root}/config/pusher.yml")
+PUSHER = YAML.load_file("#{Rails.root}/config/pusher.yml")
 
-Pusher.app_id = keys['app_id']
-Pusher.key = keys['key']
-Pusher.secret = keys['secret']
+Pusher.app_id = PUSHER['app_id']
+Pusher.key = PUSHER['key']
+Pusher.secret = PUSHER['secret']

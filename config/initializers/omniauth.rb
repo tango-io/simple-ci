096699ee8b0f7e06687f18a07c43463dba82272d
Cesar Gomez <cesargomez89@gmail.com>
2013-10-28 14:51:55 -0600

    changed to constant omniauth yaml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  config = YAML.load_file("#{Rails.root}/config/github_credentials.yml")[Rails.env]
-  provider :github, config['client_id'], config['client_secret']
+  CONFIG = YAML.load_file("#{Rails.root}/config/github_credentials.yml")[Rails.env]
+  provider :github, CONFIG['client_id'], CONFIG['client_secret']
 end

b33a8cea45cc000dcfed270b0b89ecbd736403eb
Oscar Hugo Cardenas Lopez <ohcl87@hotmail.com>
2013-11-04 11:23:21 -0600

    spec to dashboard index page add new repository

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -27,12 +27,14 @@ feature 'dashboard' do
               user_id: user.id)
   end
 
+  let(:repository_list) {public_repositories}
+
   def public_repositories
     repositories = []
     5.times do
       repositories << Fabricate.build(:repository,
                                      uid: user.uid,
-                                     name: Faker::Internet.user_name,
+                                     name: Faker::Internet.domain_word,
                                      url: Faker::Internet.url,
                                      user_id: user.id)
     end
@@ -40,11 +42,23 @@ feature 'dashboard' do
   end
 
   before() do
-    User.any_instance.stub(:public_repositories).and_return(public_repositories)
+    User.any_instance.stub(:public_repositories).and_return(repository_list)
     page.set_rack_session(:user_id => user.id)
     visit dashboard_index_path
   end
 
   scenario 'local repositories list' do
+    find("tr[@id='#{repository.name}']")
+  end
+
+  scenario 'add new repository', :js do 
+    click_link 'add repositories'
+    repo = repository_list.sample()
+    within('#add-repos-modal') do
+      find("#on#{repo.name}").click
+      click_button 'Close'
+    end
+    visit dashboard_index_path
+    find("##{repo.name}")
   end
 end

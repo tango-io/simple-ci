f9261685174cc5d4310d9ff150b36664c11d2762
Cesar Gomez <cesargomez89@gmail.com>
2013-11-05 17:39:49 -0600

    unsubscribe hooks

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,7 +1,8 @@
 require 'httparty'
 class Repository < ActiveRecord::Base
   belongs_to :user
-  after_create :suscribe_hooks
+  before_create :subscribe_hooks
+  before_destroy :unsubscribe_hooks
 
   validates :name, :url, :user_id, presence: true
   validates_uniqueness_of :url
@@ -20,18 +21,23 @@ class Repository < ActiveRecord::Base
   }.to_json
 
   def hooks_url
-    "https://api.github.com/repos/#{user.nickname}/#{name}/events/"
+    "https://api.github.com/repos/#{user.nickname}/#{name}/hooks"
   end
 
-  def suscribe_hooks
-    url = "https://api.github.com/repos/#{user.nickname}/#{name}/hooks"
-    url+= "?access_token=#{user.github_token}"
+  def subscribe_hooks
+    url= hooks_url+"?access_token=#{user.github_token}"
     response = HTTParty.post(url, body: HOOK_OPTIONS)
-    if response['id'].exists?
-      update(hook_id: response['id'])
+    if response['id'].present?
+      self.hook_id = response['id']
     else
       false
     end
+  end
+
+  def unsubscribe_hooks
+    url= hooks_url+"/#{hook_id}?access_token=#{user.github_token}"
+    response = HTTParty.delete(url)
+    false unless response.nil?
   end
 
 end

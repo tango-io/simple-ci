f5ce7262e046889f22d8dba61b7155ef89f81183
Oscar Hugo Cardenas Lopez <ohcl87@hotmail.com>
2013-11-05 10:45:10 -0600

    refactor of pull request 4

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -20,42 +20,48 @@ feature 'dashboard' do
   end
 
   let!(:repository) do
-    Fabricate(:repository,
-              uid: user.uid,
-              name: Faker::Internet.user_name,
-              url: Faker::Internet.url,
-              user_id: user.id)
+    Fabricate(
+      :repository,
+      uid: user.uid,
+      name: Faker::Internet.user_name,
+      url: Faker::Internet.url,
+      user_id: user.id
+    )
   end
 
-  let(:repository_list) {public_repositories}
+  let(:repository_list) { public_repositories }
+
+  def public_repo
+    Fabricate.build(
+      :repository,
+      uid: user.uid,
+      name: Faker::Internet.domain_word,
+      url: Faker::Internet.url,
+      user_id: user.id
+    )
+  end
 
   def public_repositories
     repositories = []
-    5.times do
-      repositories << Fabricate.build(:repository,
-                                     uid: user.uid,
-                                     name: Faker::Internet.domain_word,
-                                     url: Faker::Internet.url,
-                                     user_id: user.id)
-    end
+    5.times { repositories << public_repo }
     repositories
   end
 
-  before() do
+  before do
     User.any_instance.stub(:public_repositories).and_return(repository_list)
     page.set_rack_session(:user_id => user.id)
     visit dashboard_index_path
   end
 
-  scenario 'local repositories list' do
+  scenario 'load previously added repositories ' do
     find("tr[@id='#{repository.name}']")
   end
 
-  scenario 'add new repository', :js do 
+  scenario 'add new repository', :js do
     click_link 'add repositories'
-    repo = repository_list.sample()
+    repo = repository_list.sample
     within('#add-repos-modal') do
-      find("#on#{repo.name}").click
+      find("#on_#{repo.name}").click
       click_button 'Close'
     end
     visit dashboard_index_path

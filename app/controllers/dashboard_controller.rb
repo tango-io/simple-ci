316d5e2bfe2a42d95eeb11cb5d0cbf67180e1b1a
Oscar Hugo Cardenas Lopez <ohcl87@hotmail.com>
2013-11-06 11:29:47 -0600

    create a database.yml.sample

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,4 +4,5 @@ class DashboardController < ApplicationController
   expose(:user) { current_user }
   expose(:github_repos) { current_user.public_repositories }
   expose(:local_repos) { current_user.repositories }
+
 end

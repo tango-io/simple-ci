ce4ca7b305a486f8d5928906def172afd2beb30c
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-11-14 11:57:58 -0600

    ADded more stuff

---
+++ config/initializers/libraries.rb
@@ -0,0 +1,2 @@
+require 'ci'
+require 'github'
---
+++ lib/github.rb
@@ -0,0 +1,41 @@
+require 'open-uri'
+class Github
+  attr_reader :gemfile, :script, :url
+
+  def initialize url
+    @url     = url
+    @gemfile = open(url_gemfile).read rescue nil
+    @script  = [
+      "git clone #{@url}",
+      "cd #{url.split('/').last}",
+      "bundle install --path vendor/bundle",
+    ]
+  end
+
+  TEST_ENV  = /rspec/
+  REGEXP_DB = /pg|mysql|mongo|sqllite3|mysql2|oci8|mysqlplus/
+
+  def is_valid?
+    @gemfile.nil? ? false : update_script
+  end
+
+  private
+
+  def update_script
+    unless scan_empty?(REGEXP_DB)
+      @script << 'bundle exec rake db:create'
+      @script << 'bundle exec rake db:test:prepare'
+    end
+    scan_empty?(TEST_ENV) ? @script << 'bundle exec rake test' : @script << 'bundle exec rspec'
+    true
+  end
+
+  def scan_empty? regexp
+    @gemfile.scan(regexp).empty?
+  end
+
+  def url_gemfile
+    "#{@url}/raw/master/Gemfile"
+  end
+
+end

71d2ce3205db41b790ba56ca2b5c3b80035dafc9
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-11-13 18:00:22 -0600

    Deleted unecessary files
    Conflicts:
    	config/pusher.yml.sample

--- config/initializers/pusher.rb
+++
@@ -1,9 +0,0 @@
-#
-# This app is going to use another websocket provider
-# in the near future.
-#
-PUSHER = YAML.load_file("#{Rails.root}/config/pusher.yml")
-
-Pusher.app_id = PUSHER['app_id']
-Pusher.key = PUSHER['key']
-Pusher.secret = PUSHER['secret']
